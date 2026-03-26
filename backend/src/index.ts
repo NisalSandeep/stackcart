a0c876ed20c35c2640b2e671a056d0a3248f80e1
NisalSandeep <sandeep-as22018@stu.kln.ac.lk>
2026-03-26 15:10:17 +0530

    setup backend and fronend

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { ENV } from "./config/env";
+import { clerkMiddleware } from "@clerk/express";
+
+const app = express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(clerkMiddleware()); //auth obj will be attached to the request object
+app.use(express.json()); //req.body
+app.use(express.urlencoded({ extended: true })); // parses form data
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to STACKCART API",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
+});
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is up and running on port ${ENV.PORT}`);
+});
